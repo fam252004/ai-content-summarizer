05d11499c10331cc083f910d73464edbc61406a2
fam252004 <fahim200425@gmail.com>
2026-05-24 21:44:31 +0530

    Fixed Render port

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const summaryRoutes = require("./routes/summaryRoutes");
 
 app.use("/api", summaryRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
